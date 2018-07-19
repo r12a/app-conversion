cd6da21a3c978159b37d1a7068f5a00631c500c4
r12a <ishida@w3.org>
2018-07-19 10:28:46 +0100

    v9 beta, first draft (still in devt)

---
+++ example.js
@@ -0,0 +1,14 @@
+// this text is used for the instructions and for testing
+// this page must be stored as UTF-8
+
+var example_test = `The decomposition mapping is <츠, U+11B8>, and not <0x110E, ᅳ, 11B8>.  1234 (ethiopic/latin?)
+<p>The title says ‫פעילות הבינאום, W3C‬ in Hebrew</p>
+\\nabc\\x09áßç\\tक際𐎔  
+html/xml (hex)\\x09&#x00E7;&#x0916;&#x0940;&#x570B;&#x10384;&Aacute;
+html/xml (dec)\\x09&#231;&#2326;&#2368;&#22283;&#66436;
+javascript\\x09\\u00E7\\u0916\\u0940\\u570B\\uD800\\uDF84
+js/rust/ruby\\x09\\u{E7}\\u{916}\\u{940}\\u{570B}\\u{10384}
+ruby\\x09\\x09\\u{E7 916 940 570B 10384}
+C\\x09\\x09\\u00E7\\u0916\\u0940\\u570B\\U00010384
+perl\\x09\\x09\\x{E7}\\x{916}\\x{940}\\x{570B}\\x{10384}
+CSS\\x09\\x09\\00E7 \\0916 \\0940 \\570B \\010384`
